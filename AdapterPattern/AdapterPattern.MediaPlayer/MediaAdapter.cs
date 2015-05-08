38dc504d13ebe631a56625f384fdef2e24130c50
renukadeshmukh <renud1988@gmail.com>
2015-05-08 13:01:03 -0400

    adapter pattern

---
+++ AdapterPattern/AdapterPattern.MediaPlayer/MediaAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern.MediaPlayer
+{
+    public class MediaAdapter : IWinAmpPlayer
+    {
+        public WindowsMediaPlayer wmp;
+
+        public MediaAdapter()
+        {
+            this.wmp = new WindowsMediaPlayer();
+        }
+
+        public void Play()
+        {
+            wmp.Play();
+        }
+    }
+}
---
+++ AdapterPattern/AdapterPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdapterPattern.MediaPlayer;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWinAmpPlayer p1 = new WinAmp();
+            p1.Play();
+
+            IWinAmpPlayer p = new MediaAdapter();
+            p.Play();
+
+            Console.ReadKey();
+        }
+    }
+}
